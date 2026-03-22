e3ed944795e23c2babc3a98675a8a154b038bc9b
Towfiqul Islam <towfiqulislam017399@gmail.com>
2026-03-22 21:11:30 +0600

    Initial commit on Sun, Mar 22, 2026  9:11:30 PM

---
+++ map.js
@@ -0,0 +1,8 @@
+const numbers = [1,2,3,4,5]
+const result = numbers.map((number)=>{
+  return (number + 1)
+})
+console.log(result);
+
+const result2 = numbers.map((number)=> number+2)
+console.log(result2);
